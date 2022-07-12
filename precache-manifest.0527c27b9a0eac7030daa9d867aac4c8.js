e30f3627878225c11e543649d5543c3ab46318b8
Hardy Lutula <hardylutula@gmail.com>
2022-07-12 09:21:58 +0200

    Updates

---
+++ precache-manifest.0527c27b9a0eac7030daa9d867aac4c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa6acaa2a623def57c9f",
+    "url": "/react-team-project/static/css/main.c266c8d6.chunk.css"
+  },
+  {
+    "revision": "aa6acaa2a623def57c9f",
+    "url": "/react-team-project/static/js/main.aa6acaa2.chunk.js"
+  },
+  {
+    "revision": "09b81300e0900d5ccbb8",
+    "url": "/react-team-project/static/css/1.4ed687a3.chunk.css"
+  },
+  {
+    "revision": "09b81300e0900d5ccbb8",
+    "url": "/react-team-project/static/js/1.09b81300.chunk.js"
+  },
+  {
+    "revision": "a7a9550f1650456b03da",
+    "url": "/react-team-project/static/js/runtime~main.a7a9550f.js"
+  },
+  {
+    "revision": "75071fd1e1b5bb7c9d611deaf5233904",
+    "url": "/react-team-project/index.html"
+  }
+];
